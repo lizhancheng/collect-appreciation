7c58030356b1484e640d0b67234438303cca6b40
lizhancheng <lzc163_email@163.com>
2017-03-23 10:45:08 +0800

    add vconsole and requirejs

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,36 @@ const concat = require('gulp-concat');
 
 gulp.task('compress', () => {
   return gulp.src([
+    './resources/libs/urijs/URI.min.js',
+    './resources/libs/require.js',
     './node_modules/chinese-name/index.js',
     './javascripts/*.js'
   ])
   .pipe(babel({
-    presets: ['es2015']
+    // presets: [['es2015', { loose: true }]],
+    // plugins to disable `use strict` mode because the `es2015` presets will enable strict mode
+    plugins: [
+      'check-es2015-constants',
+      'transform-es2015-arrow-functions',
+      'transform-es2015-block-scoped-functions',
+      'transform-es2015-block-scoping',
+      'transform-es2015-classes',
+      'transform-es2015-computed-properties',
+      'transform-es2015-destructuring',
+      'transform-es2015-duplicate-keys',
+      'transform-es2015-for-of',
+      'transform-es2015-function-name',
+      'transform-es2015-literals',
+      'transform-es2015-object-super',
+      'transform-es2015-parameters',
+      'transform-es2015-shorthand-properties',
+      'transform-es2015-spread',
+      'transform-es2015-sticky-regex',
+      'transform-es2015-template-literals',
+      'transform-es2015-typeof-symbol',
+      'transform-es2015-unicode-regex',
+      'transform-regenerator',
+    ]
   }))
   .pipe(concat('index.js'))
   .pipe(sourcemaps.init())
---
+++ javascripts/require.config.js
@@ -0,0 +1,19 @@
+/**
+ * requirejs config
+ */
+ requirejs.config({
+   baseUrl: '../resources/libs',
+   paths: {
+     vconsole: 'vconsole/vconsole.min',
+     uri     : 'urijs/URI.min'
+   }
+ });
+
+(() => {
+
+ const uri = URI(location.href);
+ if(uri.hasQuery('debug')) {
+
+   require(['vconsole']);
+ }
+})()
